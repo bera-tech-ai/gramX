dfa05fe3ad91a32ec16ec5d423d16120f37cac85
bera-tech-ai <beratech15@gmail.com>
2025-09-07 23:08:39 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,16 @@
 const CACHE_NAME = 'gramx-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/manifest.json',
-  'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap',
-  'https://fonts.googleapis.com/icon?family=Material+Icons'
+  '/styles.css',
+  '/app.js',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
